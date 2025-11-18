ae665d776bbaa1d371d50c12378a327d19b21503
mesomnath <somnathsahu.dev@gmail.com>
2025-11-19 01:26:52 +0530

    Reapply "Java For Loops"
    This reverts commit 8be6ce5579dac0ec90934b34b957e0a11f1dd734.

---
+++ src/javaBasics/forLoops/StarPattern.java
@@ -0,0 +1,22 @@
+package javaBasics.forLoops;
+
+public class StarPattern {
+    /*
+
+     *
+     **
+     ***
+     ****
+
+     */
+
+    public static void main(String args[]){
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
